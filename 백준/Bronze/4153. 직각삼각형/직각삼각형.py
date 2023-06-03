d86d37bf592082ad22b97c990ff0e585b38c2ff1
rnignon <86004439+rnignon@users.noreply.github.com>
2023-06-04 02:49:02 +0900

    [Bronze III] Title: 직각삼각형, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,10 @@
+def solution() :
+    while 1 :
+        a, b, c = sorted(map(int, input().split()))
+        if a == 0 : break
+        if a ** 2 + b ** 2 == c ** 2 :
+            print('right')
+        else : print('wrong')
+    
+solution()
+
