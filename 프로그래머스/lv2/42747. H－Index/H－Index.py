88c441eb063441c381b5e83857838c7ef85d6205
mignon <86004439+rnignon@users.noreply.github.com>
2023-06-12 04:20:22 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,12 +1,9 @@
 def solution(citations):
     citations = sorted(citations)
-    answer = []
-    length = max(citations)
-    for i in range(length + 1) :
-        count = 0
-        for j in citations :
-            if i <= j :
-                count += 1
-        if count >= i :
-            answer.append(i)
+    answer = [0]
+    length = len(citations)
+    for i in range(length) :
+        if citations[i] >= length - i :
+            answer.append(length - i)
+            print(length - i)
     return max(answer)
