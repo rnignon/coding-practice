e0a5ed300a5ffa3fce50dcd14ffc1dd8f4374971
rnignon <86004439+rnignon@users.noreply.github.com>
2023-07-08 14:51:59 +0900

    [Silver IV] Title: 설탕 배달, Time: 76 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,16 @@
+def solution():
+    n = int(input())
+    count = 0
+    while n > 0 :
+        if n % 5 == 0 :
+            count += n // 5
+            n = n % 5
+            break
+        else :
+            count += 1
+            n -= 3
+    if n != 0 :
+        print(-1)
+    else :
+        print(count)
+solution()
